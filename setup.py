1886cbfdba114edfda4002b58e651ffc251943dc
fadhillaha <40687068+fadhillaha@users.noreply.github.com>
2025-12-11 18:18:53 +0900

    Moving files

--- setup.py
+++ setup.py
@@ -5,6 +5,4 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[],
-    author='Kazuki Kitano',
-    description='Project aiming for auto allocation of speech bubbles',
 )
